806aa1b58b93c37f3965a05d35d2c9af2645d1ce
thanh binh <binhthanhk2@gmail.com>
2023-02-26 22:11:38 +0700

    up login session và register user

---
+++ ProgressManStudent/ProgressManStudent/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using ProgressManStudent.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection;
+using static ProgressManStudent.Models.LoginModel;
+using System.Web.UI;
+using System.Xml.Linq;
+
+namespace ProgressManStudent.Controllers
+{
+    public class LoginController : Controller
+    {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString);
+        // GET: Login
+        public ActionResult Index()
+        {
+            var test = Session["UserSuccess"];
+            if (test != null)
+            {
+                // Nếu đã tồn tại, chuyển hướng đến trang chủ
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            List<UserInfo> list = new List<UserInfo>();
+            //LoginModel loginModelValue = new LoginModel();
+            list = model.Login(model.Username,model.Password);
+            if(list.Count > 0)
+            {
+                Session["UserSuccess"] = model.Username.Trim();
+                Session["UserId"] =  list[0].MaSinhVien.Trim();
+                return RedirectToAction("Index", "Home", new {id = list[0].MaSinhVien.Trim(), name = list[0].HoTen.Trim() });
+            }
+            else
+            {
+                ViewBag.Error = "Sai tên đăng nhập hoặc mật khẩu.";
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ ProgressManStudent/ProgressManStudent/Models/LoginModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ProgressManStudent.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Tên đăng nhập không được để trống.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Mật khẩu không được để trống.")]
+        public string Password { get; set; }
+        
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString);
+        public class UserInfo
+        {
+            public string MaSinhVien { get; set; }
+            public string GioiTinh { get; set; }
+            public string LopNienChe { get; set; }
+            public string HoTen { get; set; }
+        }
+        public List<UserInfo> Login(string username, string password)
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand("spLogin", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter param1 = new SqlParameter("@tenTaiKhoan", SqlDbType.VarChar);
+                param1.Value = username;
+                command.Parameters.Add(param1);
+
+                SqlParameter param2 = new SqlParameter("@matKhau", SqlDbType.VarChar);
+                param2.Value = password;
+                command.Parameters.Add(param2);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    //result = reader[0].ToString();
+                    UserInfo user = new UserInfo();
+                    user.HoTen = (string)reader["HoTen"];
+                    user.LopNienChe = (string)reader["LopNienChe"];
+                    user.GioiTinh = (string)reader["GioiTinh"];
+                    user.MaSinhVien = (string)reader["MaSinhVien"];
+                    users.Add(user);
+                }
+                connection.Close();
+            }
+            return users;
+        }
+    }
+}
